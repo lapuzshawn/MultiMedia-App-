8b2e5734c9c449b8b99756c037b9225d9fd41d25
ltram16 <ltram16@gmail.com>
2023-04-08 17:02:54 -0700

    moved folders, created profile page prototype

---
+++ controllers/routes/profileRoutes.js
@@ -0,0 +1,93 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+async function loadUserProfile(userId) {
+  const user = await User.findOne({ where: { id: userId } });
+  return {
+    userId: user.id,
+    name: user.name,
+    username: user.username,
+    avatar: "https://www.w3schools.com/w3images/avatar1.png"
+  };
+}
+
+function loadUserSocialLinks(userId) {
+  return [
+    {
+      label: "Facebook",
+      link: "https://facebook.com",
+      icon: "fa fa-book",
+    },
+    {
+      label: "Instagram",
+      link: "https://instagram.com",
+      icon: "fa fa-book",
+    },
+    {
+      label: "Twitter",
+      link: "https://twitter.com",
+      icon: "fa fa-book",
+    },
+  ];
+}
+
+function loadUserRecentPosts(userId) {
+  return [
+    {
+      image:
+        "https://res.cloudinary.com/dkfptto8m/image/upload/v1674371886/logrocket-css-masonry/10.jpg",
+      link: "abc",
+      createdAt: 123,
+    },
+    {
+      image:
+        "https://res.cloudinary.com/dkfptto8m/image/upload/v1674344289/logrocket-css-masonry/8.jpg",
+      link: "abc",
+      createdAt: 123,
+    },
+    {
+      image:
+        "https://res.cloudinary.com/dkfptto8m/image/upload/v1674373487/logrocket-css-masonry/window.jpg",
+      link: "abc",
+      createdAt: 123,
+    },
+    {
+      image:
+        "https://res.cloudinary.com/dkfptto8m/image/upload/v1674344282/logrocket-css-masonry/4.jpg",
+      link: "abc",
+      createdAt: 123,
+    },
+    {
+      image:
+        "https://res.cloudinary.com/dkfptto8m/image/upload/v1674344280/logrocket-css-masonry/7.jpg",
+      link: "abc",
+      createdAt: 123,
+    },
+  ];
+}
+
+router.get("/:id", withAuth, async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const user = req.session;
+
+    const profile = await loadUserProfile(userId);
+    const socialLinks = await loadUserSocialLinks(userId);
+    const recentposts = await loadUserRecentPosts(userId);
+
+    res.render("pages/profile", {
+      title: "Profile",
+      user,
+      recentposts,
+      socialLinks,
+      profile,
+      viewMyProfile: user.userId === profile.userId,
+    });
+  } catch (err) {
+    res.status(400).json({ message: "Invalid Request" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // Import required Node packages
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars-hotreload');
-const routes = require('./controllers');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars-hotreload");
+const routes = require("./controllers");
 exphbs.hotreload();
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 // Initialize Express and set up port
 const app = express();
@@ -15,42 +15,43 @@ const PORT = process.env.PORT || 3001;
 
 // Create a Handlebars instance
 const hbs = exphbs.create({
-	hotreload: true,
+  hotreload: true,
 });
 
 // Set up session configuration
 const sess = {
-	secret: 'Super secret secret',
-	cookie: {
-		maxAge: 300000,
-		httpOnly: true,
-		secure: false,
-		sameSite: 'strict',
-	},
-	resave: false,
-	saveUninitialized: true,
-	store: new SequelizeStore({
-		db: sequelize,
-	}),
+  secret: "Super secret secret",
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 // Set up session middleware
 app.use(session(sess));
 
 // Set up Handlebars as the view engine
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
+app.engine("handlebars", hbs.engine);
+app.set('views', path.join(__dirname, '/views'));
 
 // Set up middleware for parsing request bodies and serving static files
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Set up routes
 app.use(routes);
 
 // Sync the database and start the server
 app.listen(PORT, () => {
-	console.log(`App listening on port ${PORT}!`);
-	sequelize.sync({ force: false });
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
 });
