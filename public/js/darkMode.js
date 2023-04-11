eed6818402944b90cfd0c0bff23da7cb7368412c
Kenneth Nilsson-Snow <51213931+knilssonsnow@users.noreply.github.com>
2023-04-10 23:08:49 -0400

    added darkmode functionality and button on nav.

---
+++ public/js/darkMode.js
@@ -0,0 +1,32 @@
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('#darkmode-toggle-label');
+
+
+const enableDarkMode = () => {
+
+    document.body.classList.add('darkMode');
+
+    localStorage.setItem('darkMode', 'enabled')
+}
+
+const disableDarkMode = () => {
+
+    document.body.classList.remove('darkMode');
+    
+    localStorage.setItem('darkMode', null)
+}
+
+// if (darkMode === 'enabled') {
+//     enableDarkMode();
+// } else {
+//     disableDarkMode();
+// }
+
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    if (darkMode !== 'disabled') {
+        disableDarkMode();
+    } else {
+        enableDarkMode();
+    }
+})
