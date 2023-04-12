e036b987361345bae5f4f2a42f1a6272acdca00e
Shawn Lapuz <lapuzshawn@gmail.com>
2023-04-11 18:06:57 -0700

    pushing admin revisions

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,6 +4,29 @@ const adminFormHandler = async function(event) {
     const nameEl = document.querySelector('#name-input-signup');
     const usernameEl = document.querySelector('#username-input-signup');
     const passwordEl = document.querySelector('#password-input-signup');
+
+    const response = await fetch('/admin', {
+        method: "PUT",
+        body: JSON.stringify({
+            name: nameEl.value,
+            username: usernameEl.value,
+            password: passwordEl.value,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to sign up');
+    }
+};
+
+const signup_form = document
+    .querySelector('#signup-form')
+if (signup_form) signup_form.addEventListener('submit', signupFormHandler);
+
+
 /*
 const adminHeader = document.querySelector(".admin-header");
 
@@ -26,23 +49,3 @@ if (!viewMyProfile) {
 adminHeader.style.display = "block";
 
 */
-    const response = await fetch('/admin', {
-        method: "PUT",
-        body: JSON.stringify({
-            name: nameEl.value,
-            username: usernameEl.value,
-            password: passwordEl.value,
-        }),
-        headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert('Failed to sign up');
-    }
-};
-
-const signup_form = document
-    .querySelector('#signup-form')
-if (signup_form) signup_form.addEventListener('submit', signupFormHandler);
