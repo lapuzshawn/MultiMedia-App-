5b3db3e60b81f91e913874acd154486932f74ec9
Kenneth Nilsson-Snow <51213931+knilssonsnow@users.noreply.github.com>
2023-04-11 23:17:28 -0400

    added missing code

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,70 +3,74 @@ const { User } = require('../../models');
 const { Op } = require('sequelize');
 
 // POST /api/users is a registration route for creating a new user
-router.post('/signup', async (req, res) => {
-	try {
-		console.log(req.body.name);
-		const newUser = await User.create(req.body);
+router.post('/', async (req, res) => {
+    try {
+        console.log(req.body.name);
+        const newUser = await User.create({
+            name: req.body.name,
+            username: req.body.username,
+            password: req.body.password,
+        });
 
-		req.session.save(() => {
-			req.session.userId = newUser.id;
-			req.session.name = newUser.name;
-			req.session.username = newUser.username;
-			req.session.loggedIn = true;
+        req.session.save(() => {
+            req.session.userId = newUser.id;
+            req.session.name = newUser.name;
+            req.session.username = newUser.username;
+            req.session.loggedIn = true;
 
-			res.json(newUser);
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+            res.json(newUser);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 // POST /api/users/login is a login route for an existing user
-router.post('/login', async (req, res) => {
-	try {
-		const user = await User.findOne({
-			where: {
-				username: req.body.username,
-			},
-		});
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
 
-		// error if no user found
-		if (!user) {
-			res.status(400).json({ message: 'No user account found!' });
-			return;
-		}
+    // error if no user found
+    if (!user) {
+        res.status(400).json({ message: 'No user account found!' });
+        return;
+    }
 
-		// check password against the db password
-		const validPassword = user.checkPassword(req.body.password);
+    // check password against the db password
+    const validPassword = user.checkPassword(req.body.password);
 
-		// error if invalid password
-		if (!validPassword) {
-			res.status(400).json({ message: 'No user account found!' });
-		}
+    // error if invalid password
+    if (!validPassword) {
+        res.status(400).json({ message: 'No user account found!'});
+    }
 
-		req.session.save(() => {
-			req.session.userId = user.id;
-			req.session.name = user.name;
-			req.session.username = user.username;
-			req.session.loggedIn = true;
+    req.session.save(() => {
+        req.session.userId = user.id;
+        req.session.name = user.name;
+        req.session.username = user.username;
+        req.session.loggedIn = true;
 
-			res.json({ user, message: 'You are now logged in!' });
-		});
-	} catch (err) {
-		res.status(400).json({ message: 'No user account found!' });
-	}
+        res.json({ user, message: 'You are now logged in!' });
+    });
+    } catch (err) {
+        res.status(400).json({ message: 'No user account found!' });
+    }
 });
 
-// POST /api/user/logout is a logout route for an existing user
+// POST /api/users/logout is a logout route for an existing user
 router.post('/logout', (req, res) => {
-	if (req.session.loggedIn) {
-		req.session.destroy(() => {
-			res.status(204).end();
-		});
-	} else {
-		res.status(404).end();
-	}
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 //get route for getting the user's name
